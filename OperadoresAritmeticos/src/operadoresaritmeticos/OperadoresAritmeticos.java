c2b5e7e9f8ad7b968176e5e22200f378cf54dad1
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-08-15 23:53:10 -0300

    OperadoresAritmeticos
    Adicionei a calculadora, testes de operadores aritmeticos, com incrementos, decrementos e uso da classe Math

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author ronal
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é:" + m); */
+        
+       // incremento 
+       
+       int numero = 10;
+       int valor =  5 + ++numero;
+        System.out.println(valor);
+        System.out.println(numero);
+                      
+       // decremento
+       
+       int num = 10;
+       int val = 5 + --numero;
+        System.out.println(num);
+        System.out.println(val);
+        
+        // atribuições
+        
+        int x = 4;
+        int y = 4;
+        x += 2;
+        y *= 2;
+        System.out.println("soma: " + x);
+        System.out.println("multiplicação: " + y);
+       
+    }
+    
+}
